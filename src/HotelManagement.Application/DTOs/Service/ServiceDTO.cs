8c612cb6333ca1825354d53a49691bc110d443fc
Tuan trinh <tuantu2k2badaoyl@gmail.com>
2021-11-09 00:18:14 +0700

    add service

---
+++ src/HotelManagement.Application/DTOs/Service/ServiceDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace HotelManagement.Application.DTOs.Service
+{
+    public class ServiceDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int ServiceTypeId { get; set; }
+    }
+}
